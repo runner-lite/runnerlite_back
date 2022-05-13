ec76bb0946a01142131657a72a6088c9ddd1ee7a
Mihail_Vorobyov <ptichkinm@mail.ru>
2022-05-13 23:45:31 +0300

    добавил в application.properties.template настройку swagger'a; изменил тип поля result в RunningResult и RunningResultDto; добавил sql-скрипт для изменений поля running_results.RESULT в БД

--- runnerlite-rest/src/main/java/ru/runnerlite/entities/dto/RunningResultDto.java
+++ runnerlite-rest/src/main/java/ru/runnerlite/entities/dto/RunningResultDto.java
@@ -3,7 +3,6 @@ package ru.runnerlite.entities.dto;
 import lombok.Data;
 
 import java.io.Serializable;
-import java.time.Instant;
 
 @Data
 public class RunningResultDto implements Serializable {
@@ -11,5 +10,5 @@ public class RunningResultDto implements Serializable {
 	private final Integer runningId;
 	private final Integer userId;
 	private final Integer finishPlace;
-	private final Instant result;
+	private final Integer result;
 }
