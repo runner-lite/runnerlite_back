c01eda252ea575d1caa2caffd052309792f86282
Pavel A. Khanin <pkhanyn@gmail.com>
2022-05-14 11:46:35 +0300

    fix AchievementRepository

--- runnerlite-rest/src/main/java/ru/runnerlite/entities/dto/MyAchievementsDto.java
+++ runnerlite-rest/src/main/java/ru/runnerlite/entities/dto/MyAchievementsDto.java
@@ -1,18 +1,22 @@
 package ru.runnerlite.entities.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.time.Instant;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MyAchievementsDto implements Serializable {
-    private final Integer runningNumber; //номер забега
-    private final Instant runningDate; //дата забега
-    private final String teamsName; //имя команды
-    private final Integer teamsId; //для логотипа команды
-    private final String achievementName; //наименование достижения
-    private final Integer achievementId; //для миниатюры достижения
-    private final Integer finishPlace; //место в забеге с достижением
-    private final Instant result; // результат (время) в забеге с достижением
+    private Integer runningNumber; //номер забега
+    private Instant runningDate; //дата забега
+    private String teamsName; //имя команды
+    private Integer teamsId; //для логотипа команды
+    private String achievementName; //наименование достижения
+    private Integer achievementId; //для миниатюры достижения
+    private Integer finishPlace; //место в забеге с достижением
+    private Integer result; // результат (время) в забеге с достижением
 }
--- runnerlite-rest/src/main/java/ru/runnerlite/repositories/AchievementRepository.java
+++ runnerlite-rest/src/main/java/ru/runnerlite/repositories/AchievementRepository.java
@@ -13,9 +13,17 @@ import java.util.List;
 @Repository
 public interface AchievementRepository extends CrudRepository<Achievement, Long> {
 
-    @Query(value = "select new ru.runnerlite.entities.dto.MyAchievementsDto(a.runningResults.teamsRunningCount.number, " +
-            "a.runningResults.teamsRunningCount.runningDate, a.runningResults.teamsRunningCount.teams.name, a.runningResults.teamsRunningCount.teams.id," +
-            "a.refAchievementsType.description, a.refAchievementsType.id, a.runningResults.finishPlace, a.runningResults.result) " +
+
+
+    @Query(value = "select new ru.runnerlite.entities.dto.MyAchievementsDto(" +
+            "a.runningResults.teamsRunningCount.number, " +
+            "a.runningResults.teamsRunningCount.runningDate, " +
+            "a.runningResults.teamsRunningCount.teams.name, " +
+            "a.runningResults.teamsRunningCount.teams.id," +
+            "a.refAchievementsType.description, " +
+            "a.refAchievementsType.id, " +
+            "a.runningResults.finishPlace, " +
+            "a.runningResults.result) " +
             "from Achievement a " +
             "where a.runningResults.secUser.email=:currentUserName")
     List<MyAchievementsDto> findAchievementByUserName(@Param("currentUserName") String currentUserName);
