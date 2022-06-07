6bdc3af8d9a0c149cab7a2a89ca69d1f0675ad51
Mihail_Vorobyov <ptichkinm@mail.ru>
2022-06-07 13:52:03 +0300

    Причесал RunningPrepareStatus

--- runnerlite-rest/src/main/java/ru/runnerlite/entities/dto/RunningPrepareStatus.java
+++ runnerlite-rest/src/main/java/ru/runnerlite/entities/dto/RunningPrepareStatus.java
@@ -10,49 +10,37 @@ public class RunningPrepareStatus {
 	private TeamsRunningCountDto running;
 	private List<TeamsVolunteerDto> needVolunteers;
 	private List<VolunteerSimpleDto> volunteers;
-	
 	private Integer recruitmentPercentage;
 	
 	public RunningPrepareStatus() {
-	}
-	
-	public RunningPrepareStatus(TeamsRunningCountDto running, List<TeamsVolunteerDto> needVolunteers, List<VolunteerSimpleDto> volunteers) {
-		this.running = running;
-		this.needVolunteers = needVolunteers;
-		this.volunteers = volunteers;
 	}
 	
 	public TeamsRunningCountDto getRunning() {
 		return running;
 	}
 	
-	public RunningPrepareStatus(TeamsRunningCountDto running, Integer recruitmentPercentage) {
-		this.running = running;
-		this.recruitmentPercentage = recruitmentPercentage;
-	}
-	
-		public List<TeamsVolunteerDto> getNeedVolunteers() {
-		return needVolunteers;
-	}
-
-	public List<VolunteerSimpleDto> getVolunteers() {
-		return volunteers;
-	}
-	
-	public Integer getRecruitmentPercentage() {
-		return recruitmentPercentage;
-	}
-	
 	public void setRunning(TeamsRunningCountDto running) {
 		this.running = running;
+	}
+	
+	public List<TeamsVolunteerDto> getNeedVolunteers() {
+		return needVolunteers;
 	}
 	
 	public void setNeedVolunteers(List<TeamsVolunteerDto> needVolunteers) {
 		this.needVolunteers = needVolunteers;
 	}
-
+	
+	public List<VolunteerSimpleDto> getVolunteers() {
+		return volunteers;
+	}
+	
 	public void setVolunteers(List<VolunteerSimpleDto> volunteers) {
 		this.volunteers = volunteers;
+	}
+
+	public Integer getRecruitmentPercentage() {
+		return recruitmentPercentage;
 	}
 	
 	public void setRecruitmentPercentage(Integer recruitmentPercentage) {
